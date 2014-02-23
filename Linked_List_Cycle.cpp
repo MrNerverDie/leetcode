1dfe1aca67a300d3ec2f1a80e5c423394494c5e1
wangxuanyi <wangxuanyihaha@gmail.com>
2014-02-23 21:38:35 +0800

    add new content

---
+++ Linked_List_Cycle.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode *p1, *p2;
+        p1 = p2 = head;
+        while(p2 != nullptr && p2->next != nullptr){
+            p1 = p1->next;
+            p2 = p2->next->next;
+            if (p1 == p2)
+                return true;
+        }
+        return false;
+    }
+};
